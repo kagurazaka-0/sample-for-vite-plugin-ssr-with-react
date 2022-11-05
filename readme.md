e35a5dbd1d38ffebf2ace78456440df5253f0201
katsuya_U <katsuya_u@icloud.com>
2022-11-05 21:00:04 +0900

    fix

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # sample-for-vite-plugin-ssr-with-react
+
+## refs
+
+- https://vite-plugin-ssr.com/
+- https://github.com/brillout/vite-plugin-ssr_vercel
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import ssr from "vite-plugin-ssr/plugin"
-import vercel from "vite-plugin-vercel"
 
 export default defineConfig({
-  plugins: [react(), ssr(), vercel()],
-  vercel: {},
+  plugins: [react(), ssr()],
 })
