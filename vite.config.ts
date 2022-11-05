d6d9e00bade2ede9613bfe2a61619b8fcb246718
katsuya_U <katsuya_u@icloud.com>
2022-11-05 20:52:32 +0900

    add vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import ssr from "vite-plugin-ssr/plugin"
+import vercel from "vite-plugin-vercel"
 
-export default {
-  plugins: [react(), ssr()],
-}
+export default defineConfig({
+  plugins: [react(), ssr(), vercel()],
+  vercel: {},
+})
