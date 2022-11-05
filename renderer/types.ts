3ff133b63dedcab6d2974cfa3f89811b464ce46e
katsuya_U <katsuya_u@icloud.com>
2022-11-05 18:19:28 +0900

    add types

---
+++ renderer/types.ts
@@ -0,0 +1,17 @@
+import type { PageContextBuiltIn } from "vite-plugin-ssr"
+import type { PageContextBuiltInClient } from "vite-plugin-ssr/client/router" // When using Client Routing
+// import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client' // When using Server Routing
+
+type Page = (pageProps: PageProps) => React.ReactElement
+type PageProps = Record<string, unknown>
+
+export type PageContextCustom = {
+  Page: Page
+  pageProps?: PageProps
+  userAgent?: string
+}
+
+export type PageContextServer = PageContextBuiltIn<Page> & PageContextCustom
+export type PageContextClient = PageContextBuiltInClient<Page> & PageContextCustom
+
+type PageContext = PageContextClient | PageContextServer
