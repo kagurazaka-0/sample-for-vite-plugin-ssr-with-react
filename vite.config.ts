485b5adc146fc10e62ff69191380d9f701f2df26
katsuya_U <katsuya_u@icloud.com>
2022-11-05 21:13:01 +0900

    add @magne4000/vite-plugin-vercel-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import vercelSsr from "@magne4000/vite-plugin-vercel-ssr"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import ssr from "vite-plugin-ssr/plugin"
 import vercel from "vite-plugin-vercel"
 
 export default defineConfig({
-  plugins: [react(), ssr(), vercel()],
+  plugins: [react(), ssr(), vercel(), vercelSsr()],
   vercel: {},
 })
